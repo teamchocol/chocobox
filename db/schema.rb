fa6aa00175f8961e79612ec763ed86a241bd9143
mnhy613 <makomakoto5678@gmail.com>
2020-04-16 05:48:33 +0000

    ログイン機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_16_034140) do
+ActiveRecord::Schema.define(version: 2020_04_16_053735) do
 
   create_table "brands", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -21,8 +21,6 @@ ActiveRecord::Schema.define(version: 2020_04_16_034140) do
   end
 
   create_table "chocolates", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "name"
     t.text "url"
     t.string "image_url"
@@ -34,6 +32,8 @@ ActiveRecord::Schema.define(version: 2020_04_16_034140) do
     t.integer "taste"
     t.integer "healthy"
     t.integer "cost_performance"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "comments", force: :cascade do |t|
@@ -72,7 +72,6 @@ ActiveRecord::Schema.define(version: 2020_04_16_034140) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
     t.string "password_digest"
     t.string "nickname"
     t.integer "age"
@@ -83,6 +82,19 @@ ActiveRecord::Schema.define(version: 2020_04_16_034140) do
     t.string "profile_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index [nil], name: "index_users_on_unlock_token", unique: true
   end
 
 end
