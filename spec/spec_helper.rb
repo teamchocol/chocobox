439d09b5b7c40182a821c4712f6fa01c814c934f
mnhy613 <makomakoto5678@gmail.com>
2020-05-30 21:57:14 +0900

    test修正

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,7 +25,7 @@ RSpec.configure do |config|
   config.verbose_retry = true
   config.display_try_failure_messages = true
   config.around :each, :js do |ex|
-    ex.run_with_retry retry: 5
+    ex.run_with_retry retry: 15
   end
   config.retry_callback = proc do |ex|
     if ex.metadata[:js]
