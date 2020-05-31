dbcf944bbb9fd2ae02fd0beb7e014ec16e4cf49e
mnhy613 <makomakoto5678@gmail.com>
2020-05-31 13:30:23 +0900

    お問い合わせページコントローラ修正

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,16 +4,16 @@ class ContactsController < ApplicationController
     @contact = Contact.new
   end
 
-  def create 
+  def create
     @contact = current_user.contacts.build(contact_params)
-      if @contact.save
-        ContactMailer.creation_email(@contact).deliver_now
-        flash[:success] = 'お問い合わせを送信しました'
-        redirect_to contacts_path
-      else
-        flash.now[:danger] = 'お問い合わせの送信に失敗しました'
-        render :new
-      end
+    if @contact.save
+      ContactMailer.creation_email(@contact).deliver_now
+      flash[:success] = 'お問い合わせを送信しました'
+      redirect_to contacts_path
+    else
+      flash.now[:danger] = 'お問い合わせの送信に失敗しました'
+      render :new
+    end
   end
 
   private
