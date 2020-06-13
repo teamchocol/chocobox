73efd5d231a1f116dc7d2ca5778e985d623b5bab
mnhy613 <makomakoto5678@gmail.com>
2020-06-14 00:08:59 +0900

    トップページのスライドショーとコメント一覧の削除を修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require activestorage
 
 //= require jquery3
+//= require jquery_ujs
 //= require popper
 //= require bootstrap
 //= require bootstrap-sprockets
@@ -21,53 +22,54 @@
 //= require jquery-cropper.min.js
 //= require activestorage
 //= require turbolinks
-//= require jquery_ujs
 //= require Chart.min
+//= require slick.min.js
 //= require_tree .
 
-$(function(){
-  $('theTarget').slick({
-      // アクセシビリティ。左右ボタンで画像の切り替えをできるかどうか
-      accessibility: true,
-      // 自動再生。trueで自動再生される。
-      autoplay: true,
-      // 自動再生で切り替えをする時間
-      autoplaySpeed: 100000,
-      // 自動再生や左右の矢印でスライドするスピード
-      speed: 100,
-      // 自動再生時にスライドのエリアにマウスオンで一時停止するかどうか
-      pauseOnHover: false,
-      // 自動再生時にドットにマウスオンで一時停止するかどうか
-      pauseOnDotsHover: true,
-      // 切り替えのアニメーション。ease,linear,ease-in,ease-out,ease-in-out
-      cssEase: 'ease',
-      // 画像下のドット（ページ送り）を表示
-      dots: false,
-      // 切り替え時のフェードイン設定。trueでon
-      fade: false,
-      // 左右の次へ、前へボタンを表示するかどうか
-      arrows: true,
-      // 無限スクロールにするかどうか。最後の画像の次は最初の画像が表示される。
-      infinite: true,
-      // 最初のスライダーの位置
-      initialSlide: 10,
-      // 画像の遅延表示。‘ondemand’or'progressive'
-      lazyLoad: 'ondemand',
-      // スライドのエリアにマウスオーバーしている間、自動再生を止めるかどうか。
-      pauseOnHover: true,
-      // スライドのエリアに画像がいくつ表示されるかを指定
-      slidesToScroll: 1,
-      // タッチスワイプに対応するかどうか
-      swipe: true,
-      // 縦方向へのスライド
-      vertical: false,
-      // 表示中の画像を中央へ
-      centerMode: true,
-      // 中央のpadding
-      centerPadding: '160px'
-    })
-  });
 
+$(document).on('turbolinks:load', function(){
+  $('theTarget').slick({
+    // アクセシビリティ。左右ボタンで画像の切り替えをできるかどうか
+    accessibility: true,
+    // 自動再生。trueで自動再生される。
+    autoplay: true,
+    // 自動再生で切り替えをする時間
+    autoplaySpeed: 100000,
+    // 自動再生や左右の矢印でスライドするスピード
+    speed: 100,
+    // 自動再生時にスライドのエリアにマウスオンで一時停止するかどうか
+    pauseOnHover: false,
+    // 自動再生時にドットにマウスオンで一時停止するかどうか
+    pauseOnDotsHover: true,
+    // 切り替えのアニメーション。ease,linear,ease-in,ease-out,ease-in-out
+    cssEase: 'ease',
+    // 画像下のドット（ページ送り）を表示
+    dots: true,
+    // 切り替え時のフェードイン設定。trueでon
+    fade: false,
+    // 左右の次へ、前へボタンを表示するかどうか
+    arrows: true,
+    // 無限スクロールにするかどうか。最後の画像の次は最初の画像が表示される。
+    infinite: true,
+    // 最初のスライダーの位置
+    initialSlide: 10,
+    // 画像の遅延表示。‘ondemand’or'progressive'
+    lazyLoad: 'ondemand',
+    // スライドのエリアにマウスオーバーしている間、自動再生を止めるかどうか。
+    pauseOnHover: true,
+    // スライドのエリアに画像がいくつ表示されるかを指定
+    slidesToScroll: 1,
+    // タッチスワイプに対応するかどうか
+    swipe: true,
+    // 縦方向へのスライド
+    vertical: false,
+    // 表示中の画像を中央へ
+    centerMode: true,
+    // 中央のpadding
+    centerPadding: '160px'
+  })
+});
+  
   $(function(){
     var back = $('#back');
     // ボタン非表示
