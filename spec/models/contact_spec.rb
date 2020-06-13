4be1ab75c5285d5810736ddcfbb2d162b89ea5b7
mnhy613 <makomakoto5678@gmail.com>
2020-06-13 18:00:42 +0900

    総合評価を非同期で表示させた

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,51 +1,51 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Contact, type: :model do
-#   describe 'お問い合わせが有効である時' do
-#     it '新規登録できること' do
-#       contact = FactoryBot.build(:contact)
-#       contact.valid?
-#       expect(contact).to be_valid
-#     end
-#   end
+RSpec.describe Contact, type: :model do
+  describe 'お問い合わせが有効である時' do
+    it '新規登録できること' do
+      contact = FactoryBot.build(:contact)
+      contact.valid?
+      expect(contact).to be_valid
+    end
+  end
 
-#   describe 'お名前を検証する場合' do
-#     it 'お名前が無いと新規登録できないこと' do
-#       user = FactoryBot.build(:contact, name: nil)
-#       user.valid?
-#       expect(user.errors[:name]).to include('を入力してください')
-#     end
+  describe 'お名前を検証する場合' do
+    it 'お名前が無いと新規登録できないこと' do
+      user = FactoryBot.build(:contact, name: nil)
+      user.valid?
+      expect(user.errors[:name]).to include('を入力してください')
+    end
 
-#     it 'お名前が50文字なら有効な状態であること' do
-#       user = FactoryBot.build(:contact, name: 'a' * 50)
-#       user.valid?
-#       expect(user).to be_valid
-#     end
+    it 'お名前が50文字なら有効な状態であること' do
+      user = FactoryBot.build(:contact, name: 'a' * 50)
+      user.valid?
+      expect(user).to be_valid
+    end
 
-#     it 'お名前が50文字超なら無効な状態であること' do
-#       user = FactoryBot.build(:contact, name: 'a' * 51)
-#       user.valid?
-#       expect(user.errors[:name]).to include('は50文字以内で入力してください')
-#     end
-#   end
+    it 'お名前が50文字超なら無効な状態であること' do
+      user = FactoryBot.build(:contact, name: 'a' * 51)
+      user.valid?
+      expect(user.errors[:name]).to include('は50文字以内で入力してください')
+    end
+  end
 
-#   describe 'メールアドレスを検証する場合' do
-#     it 'メールアドレスが無いと新規登録できないこと' do
-#       user = FactoryBot.build(:contact, email: nil)
-#       user.valid?
-#       expect(user.errors[:email]).to include('を入力してください')
-#     end
+  describe 'メールアドレスを検証する場合' do
+    it 'メールアドレスが無いと新規登録できないこと' do
+      user = FactoryBot.build(:contact, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include('を入力してください')
+    end
 
-#     it 'メールアドレスに@が含まれていないなら無効な状態であること' do
-#       user = FactoryBot.build(:contact, email: 'chocobox.jp')
-#       user.valid?
-#       expect(user.errors[:email]).to include('は不正な値です')
-#     end
+    it 'メールアドレスに@が含まれていないなら無効な状態であること' do
+      user = FactoryBot.build(:contact, email: 'chocobox.jp')
+      user.valid?
+      expect(user.errors[:email]).to include('は不正な値です')
+    end
 
-#     it 'メールアドレスが保存される前に小文字に変換されること' do
-#       user = FactoryBot.build(:contact, email: 'TESTADD@chocobox.jp')
-#       user.save
-#       expect(user.email).to eq 'testadd@chocobox.jp'
-#     end
-#   end
-# end
+    it 'メールアドレスが保存される前に小文字に変換されること' do
+      user = FactoryBot.build(:contact, email: 'TESTADD@chocobox.jp')
+      user.save
+      expect(user.email).to eq 'testadd@chocobox.jp'
+    end
+  end
+end
