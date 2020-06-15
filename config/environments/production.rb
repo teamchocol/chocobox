e73cc0448bbf36aee212c1a0d1d0edd0494d8aca
mnhy613 <makomakoto5678@gmail.com>
2020-06-15 21:27:19 +0900

    production.rb編集　メール機能

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,19 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "chocobox_#{Rails.env}"
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+      port: 587,
+      address: 'smtp.gmail.com',
+      domain: 'smtp.gmail.com',
+      user_name: ENV['EMAIL_HIDDEN'],
+      password: ENV['MAIL_PASSWORD'],
+      authentication: 'plain',
+      enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
