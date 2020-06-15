06c702217f4bcf9416301a426c74271c02d6cff7
mnhy613 <makomakoto5678@gmail.com>
2020-06-15 15:45:37 +0900

    トップページに最新のコメントをのせた

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,6 +11,26 @@ class HomeController < ApplicationController
     end
     @items = Kaminari.paginate_array(@items_full).page(params[:page]).per(3)
     @chocolate = Chocolate.new
+
+    @users = User.all
+    @comments = Comment.page(params[:page]).without_count.per(4).order(created_at: :desc)
+    @name = {}
+    @image = {}
+    @comments.each do |comment|
+      item_code = comment.item_code
+      if item_code.present?
+        chocolate = Rakuten.get_item(item_code)
+        next if chocolate["Items"].blank?
+        image_url = chocolate["Items"][0]["Item"]["mediumImageUrls"][0]["imageUrl"]
+        item_name = chocolate["Items"][0]["Item"]["itemName"]
+        if image_url.present?
+          @image[item_code] = image_url
+        end
+        if item_name.present?
+          @name[item_code] = item_name
+        end
+      end
+    end
   end
 
   def about
